a0edee31eed3ab376b80fe2cf858c2667a41630b
Santiago Narvaez <santi_narvaez_98@hotmail.com>
2023-09-07 17:07:22 -0500

    implements use cases

---
+++ app/src/main/java/co/com/credibanco/domain/use_case/GetAuthorizationWithReceiptIdUseCase.kt
@@ -0,0 +1,28 @@
+package co.com.credibanco.domain.use_case
+
+import co.com.credibanco.di.DefaultDispatcher
+import co.com.credibanco.domain.model.Authorization
+import co.com.credibanco.domain.repository.AuthorizationRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
+
+class GetAuthorizationWithReceiptIdUseCase @Inject constructor(
+    private val repository: AuthorizationRepository,
+    @DefaultDispatcher coroutineDispatcher: CoroutineDispatcher
+) : SuspendUseCase<GetAuthorizationWithReceiptIdUseCaseParams, GetAuthorizationWithReceiptIdUseCaseResult>(
+    coroutineDispatcher
+) {
+
+    override suspend fun execute(parameters: GetAuthorizationWithReceiptIdUseCaseParams) =
+        GetAuthorizationWithReceiptIdUseCaseResult(
+            repository.getAuthorizedTransactionWithReceiptId(parameters.receiptId)
+        )
+}
+
+data class GetAuthorizationWithReceiptIdUseCaseParams(
+    val receiptId: String
+)
+
+data class GetAuthorizationWithReceiptIdUseCaseResult(
+    val authorizations: Authorization?
+)
