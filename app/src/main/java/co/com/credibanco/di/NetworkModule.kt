beb5365724eb85a42f65efa04a16fd5dc85e83ff
Santiago Narvaez <santi_narvaez_98@hotmail.com>
2023-09-07 22:33:16 -0500

    Add login screen

--- app/src/main/java/co/com/credibanco/di/NetworkModule.kt
+++ app/src/main/java/co/com/credibanco/di/NetworkModule.kt
@@ -1,6 +1,7 @@
 package co.com.credibanco.di
 
 import co.com.credibanco.data.ApiService
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Converter
@@ -8,6 +9,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkModule {
+
+    private const val HEADER_NAME = "Authorization"
+    private const val HEADER_VALUE = "Basic MDAwMTIzMDAwQUJD"
 
     private var apiService: ApiService? = null
 
@@ -24,13 +28,24 @@ object NetworkModule {
     }
 
     private fun createOkHttpClient(): OkHttpClient {
-        val loggingInterceptor =
-            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        val loggingInterceptor = HttpLoggingInterceptor()
+            .setLevel(
+                HttpLoggingInterceptor.Level.BODY
+            )
 
-        return OkHttpClient.Builder().apply {
-            cache(null)
-            addInterceptor(loggingInterceptor)
-        }.build()
+        return OkHttpClient.Builder()
+            .cache(null)
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor { chain: Interceptor.Chain ->
+                val request = chain.request()
+
+                val newRequest = request.newBuilder()
+                    .addHeader(HEADER_NAME, HEADER_VALUE)
+                    .build()
+
+                chain.proceed(newRequest)
+            }
+            .build()
     }
 
     private fun createConverterFactory(): Converter.Factory = GsonConverterFactory.create()
