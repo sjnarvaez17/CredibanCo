521c0ee450e8c23f7b2cc430b435bfc18cf777ba
Santiago Narvaez <santi_narvaez_98@hotmail.com>
2023-09-07 23:06:21 -0500

    Add loging functions on local data source

--- app/src/main/java/co/com/credibanco/data/source/local/LocalDataSource.kt
+++ app/src/main/java/co/com/credibanco/data/source/local/LocalDataSource.kt
@@ -1,8 +1,16 @@
 package co.com.credibanco.data.source.local
 
 import co.com.credibanco.data.source.local.entities.AuthorizationEntity
+import co.com.credibanco.data.source.local.entities.LoginEntity
 
 interface LocalDataSource {
+
+    suspend fun saveCredentials(loginEntity: LoginEntity)
+
+    suspend fun getCredentials(
+        commerceCode: String,
+        terminalCode: String
+    ): List<LoginEntity>
 
     suspend fun saveAuthorization(vararg authorizationEntity: AuthorizationEntity)
 
